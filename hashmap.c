81bb5a397b4c8612e8f87d0cbba3db1d3d77fb12
Replit user <>
2022-09-10 02:37:34 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -76,7 +76,7 @@ void eraseMap(HashMap * map,  char * key) {
   long index =hash(key, map->capacity);
   while (map->buckets[index] != NULL && map->buckets[index]->key != NULL){
     if (is_equal(key, map->buckets[index]->key) == 1){
-    map->buckets[index]->key;
+    map->buckets[index]->key=NULL;
     return elemento;
     }
     index= (index+1)% map->capacity;
